78c066a9fd2220ec8d45f44edfadf9343f35cb67
Matt Thomson <red.cataclysm@gmail.com>
2017-11-12 00:21:21 -0500

    Skeleton

---
+++ src/OrbitControls.ts
@@ -0,0 +1,167 @@
+import * as THREE from 'three';
+
+export default class OrbitControls extends THREE.EventDispatcher{
+  constructor() {
+    super();
+  }
+
+  getPolarAngle() {
+
+  }
+
+  getAzimuthalAngle() {
+
+  }
+
+  saveState() {
+
+  }
+
+  reset() {
+
+  }
+
+  update() {
+
+  }
+
+  dispose() {
+
+  }
+}
+
+function getAutoRotationAngle() {
+
+}
+
+function getZoomScale() {
+
+}
+
+function rotateLeft(angle) {
+
+}
+
+function rotateUp(angle) {
+
+}
+
+function panLeft() {
+
+}
+
+function panUp() {
+
+}
+
+function pan() {
+
+}
+
+function dollyIn(dollyScale) {
+
+}
+
+function dollyOut(dollyScale) {
+
+}
+
+function handleMouseDownRotate(event) {
+
+}
+
+function handleMouseDownDolly(event) {
+
+}
+
+function handleMouseDownPan(event) {
+
+}
+
+function handleMouseMoveRotate(event) {
+
+}
+
+function handleMouseMoveDolly(event) {
+
+}
+
+function handleMouseMovePan(event) {
+
+}
+
+function handleMouseUp(event) {
+
+}
+
+function handleMouseWheel(event) {
+
+}
+
+function handleKeyDown(event) {
+
+}
+
+function handleTouchStartRotate(event) {
+
+}
+
+function handleTouchStartDolly(event) {
+
+}
+
+function handleTouchStartPan(event) {
+
+}
+
+function handleTouchMoveRotate(event) {
+
+}
+
+function handleTouchMoveDolly(event) {
+
+}
+
+function handleTouchMovePan(event) {
+
+}
+
+function handleTouchEnd(event) {
+
+}
+
+function onMouseDown(event) {
+
+}
+
+function onMouseMove(event) {
+
+}
+
+function onMouseUp(event) {
+
+}
+
+function onMouseWheel(event) {
+
+}
+
+function onKeyDown(event) {
+
+}
+
+function onTouchStart(event) {
+
+}
+
+function onTouchMove(event) {
+
+}
+
+function onTouchEnd(event) {
+
+}
+
+function onContextMenu(event) {
+
+}
