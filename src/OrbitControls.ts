cf03011ba4facd670f8b8b2d241064cf0c285812
Matt Thomson <red.cataclysm@gmail.com>
2017-11-12 00:47:25 -0500

    Default Settings

--- src/OrbitControls.ts
+++ src/OrbitControls.ts
@@ -1,8 +1,157 @@
+/**
+ * @author qiao / https://github.com/qiao
+ * @author mrdoob / http://mrdoob.com
+ * @author alteredq / http://alteredqualia.com/
+ * @author WestLangley / http://github.com/WestLangley
+ * @author erich666 / http://erichaines.com
+ * @author redice44 / https://github.com/redice44
+ */
+
+// This set of controls performs orbiting, dollying (zooming), and panning.
+// Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
+//
+//    Orbit - left mouse / touch: one finger move
+//    Zoom - middle mouse, or mousewheel / touch: two finger spread or squish
+//    Pan - right mouse, or arrow keys / touch: three finger swipe
+
 import * as THREE from 'three';
 
-export default class OrbitControls extends THREE.EventDispatcher{
-  constructor() {
+interface ArrowKeys {
+  LEFT: number,
+  UP: number,
+  RIGHT: number,
+  BOTTOM: number
+}
+
+interface MouseButtons {
+  ORBIT: THREE.MOUSE,
+  ZOOM: THREE.MOUSE,
+  PAN: THREE.MOUSE
+}
+
+export default class OrbitControls extends THREE.EventDispatcher {
+  // Set to false to disable this control
+  public enabled: boolean;
+
+  // "target" sets the location of focus, where the object orbits around
+  public target: THREE.Vector3;
+
+  // How far you can dolly in and out ( PerspectiveCamera only )
+  public minDistance: number;
+  public maxDistance: number;
+
+  // How far you can zoom in and out ( OrthographicCamera only )
+  public minZoom: number;
+  public maxZoom: number;
+
+  // How far you can orbit vertically, upper and lower limits.
+  // Range is 0 to Math.PI radians.
+  public minPolarAngle: number; // radians
+  public maxPolarAngle: number; // radians
+
+  // How far you can orbit horizontally, upper and lower limits.
+  // If set, must be a sub-interval of the interval [ - Math.PI, Math.PI ].
+  public minAzimuthAngle: number; // radians
+  public maxAzimuthAngle: number ; // radians
+
+  // Set to true to enable damping (inertia)
+  // If damping is enabled, you must call controls.update() in your animation loop
+  public enableDamping: boolean;
+  public dampingFactor: number;
+
+  // This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
+  // Set to false to disable zooming
+  public enableZoom: boolean;
+  public zoomSpeed: number;
+
+  // Set to false to disable rotating
+  public enableRotate: boolean;
+  public rotateSpeed: number;
+
+  // Set to false to disable panning
+  public enablePan: boolean;
+  public keyPanSpeed: number;  // pixels moved per arrow key push
+
+  // Set to true to automatically rotate around the target
+  // If auto-rotate is enabled, you must call controls.update() in your animation loop
+  public autoRotate: boolean;
+  public autoRotateSpeed: number; // 30 seconds per round when fps is 60
+
+  // Set to false to disable use of the keys
+  public enableKeys: boolean;
+
+  // The four arrow keys
+  public keys: ArrowKeys;
+
+  // Mouse buttons
+  public mouseButtons: MouseButtons;
+
+  private object: THREE.PerspectiveCamera | THREE.OrthographicCamera;
+  private domElement: Document | Element;
+
+  constructor(object: THREE.PerspectiveCamera | THREE.OrthographicCamera, domElement: Document | Element = document) {
     super();
+
+    this.object = object;
+    this.domElement = domElement;
+
+    // Default Settings
+
+    // Set to false to disable this control
+    this.enabled = true;
+
+    // "target" sets the location of focus, where the object orbits around
+    this.target = new THREE.Vector3()
+
+    // How far you can dolly in and out ( PerspectiveCamera only )
+    this.minDistance = 0;
+    this.maxDistance = Infinity;
+
+    // How far you can zoom in and out ( OrthographicCamera only )
+    this.minZoom = 0;
+    this.maxZoom = Infinity;
+
+    // How far you can orbit vertically, upper and lower limits.
+    // Range is 0 to Math.PI radians.
+    this.minPolarAngle = 0; // radians
+    this.maxPolarAngle = Math.PI; // radians
+
+    // How far you can orbit horizontally, upper and lower limits.
+    // If set, must be a sub-interval of the interval [ - Math.PI, Math.PI ].
+    this.minAzimuthAngle = - Infinity; // radians
+    this.maxAzimuthAngle = Infinity; // radians
+
+    // Set to true to enable damping (inertia)
+    // If damping is enabled, you must call controls.update() in your animation loop
+    this.enableDamping = false;
+    this.dampingFactor = 0.25;
+
+    // This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
+    // Set to false to disable zooming
+    this.enableZoom = true;
+    this.zoomSpeed = 1.0;
+
+    // Set to false to disable rotating
+    this.enableRotate = true;
+    this.rotateSpeed = 1.0;
+
+    // Set to false to disable panning
+    this.enablePan = true;
+    this.keyPanSpeed = 7.0;  // pixels moved per arrow key push
+
+    // Set to true to automatically rotate around the target
+    // If auto-rotate is enabled, you must call controls.update() in your animation loop
+    this.autoRotate = false;
+    this.autoRotateSpeed = 2.0; // 30 seconds per round when fps is 60
+
+    // Set to false to disable use of the keys
+    this.enableKeys = true;
+
+    // The four arrow keys
+    this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
+
+    // Mouse buttons
+    this.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.RIGHT };
   }
 
   getPolarAngle() {
@@ -28,7 +177,9 @@ export default class OrbitControls extends THREE.EventDispatcher{
   dispose() {
 
   }
+
 }
+
 
 function getAutoRotationAngle() {
 
